6030a1c459cda8884c60934a77812bfe40335cd9
learmond <jaundice.el@gmail.com>
2013-02-25 16:58:58 -0700

    committing final version of problem 1

--- problem1.py
+++ problem1.py
@@ -2,11 +2,9 @@
 
 
 result = 0
-count = 3
 
-while count < 1001:
-	if (count % 3) == 0 || (count % 5) == 0:
+for count in range(3,1000):
+	if (count % 3 == 0) or (count % 5 == 0):
 		 result += count
-	count++
 	
-print "result == " + count
+print "result == ", result  
